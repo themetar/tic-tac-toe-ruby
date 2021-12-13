09d4f03b70d8316d11c6c2adee49169c192e4731
Dimitar Apostolovski <8737118+themetar@users.noreply.github.com>
2021-12-13 10:39:19 +0100

    Add #place_mark

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -13,4 +13,30 @@ describe Board do
       expect(Board.new.to_s).to eq(expected)
     end
   end
+
+  describe '#place_mark' do
+    it 'places a mark in a specified cell' do
+      brd = Board.new
+      
+      brd.place_mark(2, 'x')
+      expected =
+                " 1 | x | 3 \n"\
+                "-----------\n"\
+                " 4 | 5 | 6 \n"\
+                "-----------\n"\
+                " 7 | 8 | 9 "
+        
+      expect(brd.to_s).to eq(expected)
+
+      brd.place_mark(4, 'o')
+      expected =
+                " 1 | x | 3 \n"\
+                "-----------\n"\
+                " o | 5 | 6 \n"\
+                "-----------\n"\
+                " 7 | 8 | 9 "
+
+      expect(brd.to_s).to eq(expected)
+    end
+  end
 end
--- src/board.rb
+++ src/board.rb
@@ -3,6 +3,10 @@ class Board
     @fields = Array.new(9, :empty)
   end
 
+  def place_mark(location, mark)
+    @fields[location - 1] = mark
+  end
+
   def to_s
     @fields
       .each_with_index.map { |mark, i| o = mark == :empty && (i + 1) || mark; " #{o} " }
