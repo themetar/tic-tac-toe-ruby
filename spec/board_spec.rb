8ec18c888537bfda2b21923ea54d266d4007b818
Dimitar Apostolovski <8737118+themetar@users.noreply.github.com>
2021-12-17 20:53:10 +0100

    Add Board#winner

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -61,4 +61,98 @@ describe Board do
       expect(board.available_cells).to eq([])
     end
   end
+
+  describe '#winner' do
+    let(:board) { Board.new }
+
+    it 'returns :empty when the game is not completed' do
+      board.place_mark(1, 'a')
+      board.place_mark(3, 'a')
+      board.place_mark(4, 'a')
+
+      expect(board.winner).to eq(:empty)
+    end
+
+    context 'when player completes a row' do
+      before do
+        board.place_mark(1, 'a')
+        board.place_mark(2, 'a')
+        board.place_mark(4, 'b')
+        board.place_mark(5, 'b')
+        board.place_mark(7, 'c')
+        board.place_mark(8, 'c')
+      end
+
+      it 'correctly returns for the first row' do
+        board.place_mark(3, 'a')
+        expect(board.winner).to eq('a')
+      end
+
+      it 'correctly returns for the second row' do
+        board.place_mark(6, 'b')
+        expect(board.winner).to eq('b')
+      end
+
+      it 'correctly returns for the third row' do
+        board.place_mark(9, 'c')
+        expect(board.winner).to eq('c')
+      end
+    end
+
+    context 'when player completes a column' do
+      before do
+        board.place_mark(1, 'a')
+        board.place_mark(7, 'a')
+        board.place_mark(2, 'b')
+        board.place_mark(8, 'b')
+        board.place_mark(3, 'c')
+        board.place_mark(9, 'c')
+      end
+
+      it 'correctly returns for the first column' do
+        board.place_mark(4, 'a')
+        expect(board.winner).to eq('a')
+      end
+
+      it 'correctly returns for the second column' do
+        board.place_mark(5, 'b')
+        expect(board.winner).to eq('b')
+      end
+
+      it 'correctly returns for the third column' do
+        board.place_mark(6, 'c')
+        expect(board.winner).to eq('c')
+      end
+    end
+
+    context 'when player completes a diagonal' do
+      before do
+        board.place_mark(1, 'a')
+        board.place_mark(9, 'a')
+        board.place_mark(3, 'b')
+        board.place_mark(7, 'b')
+      end
+
+      it 'correctly returns for the primary diagonal' do
+        board.place_mark(5, 'a')
+        expect(board.winner).to eq('a')
+      end
+
+      it 'correctly returns for the secondary diagonal' do
+        board.place_mark(5, 'b')
+        expect(board.winner).to eq('b')
+      end
+    end
+
+    it 'returns :empty when the game is tied (fully filled)' do
+      # x o x
+      # o o x
+      # x x o
+      %w(x o x o o x x x o).each_with_index do |mark, index|
+        board.place_mark(index + 1, mark)
+      end
+
+      expect(board.winner).to eq(:empty)
+    end
+  end
 end
--- src/board.rb
+++ src/board.rb
@@ -12,10 +12,37 @@ class Board
            .map { |_, i| i + 1 }
   end
 
+  def winner
+    rows = @fields.each_slice(3)
+    columns = (0..2).map { |c| select_by_sequence(@fields, (c..8).step(3)) }
+    diagonals = [
+      select_by_sequence(@fields, (0..8).step(4)),
+      select_by_sequence(@fields, (2..6).step(2))
+    ]
+
+    found = (rows + columns + diagonals).each.lazy.map { |line| line_winner(line) }
+                               .find { |winner| winner != :empty }
+
+    found || :empty
+  end
+
   def to_s
     @fields
       .each_with_index.map { |mark, i| o = mark == :empty && (i + 1) || mark; " #{o} " }
       .each_slice(3).map { |row| row.join('|') }
       .join("\n-----------\n")
+  end
+
+  private
+
+  def select_by_sequence(indexable, sequence)
+    sequence.map { |index| indexable[index] }
+  end
+  
+  def line_winner(line)
+    return line.first if line.all? { |mark| mark == line.first }
+    
+    # otherwise return
+    :empty
   end
 end
