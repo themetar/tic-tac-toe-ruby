fbf4b8f6ff149d982cee6a35512cfc30facc70e4
Dimitar Apostolovski <8737118+themetar@users.noreply.github.com>
2021-12-24 15:31:27 +0100

    Refactor Utils::prompt to style guide

--- src/utils.rb
+++ src/utils.rb
@@ -1,19 +1,21 @@
+# Place to hold utility functions
 module Utils
-
   ##
   # Prints query to standard oputput and get user input.
   #
   # If a block is given, checks acceptance with the (bool) result of the block.
   # Loops untill acceptable value is inputed.
-  def self.prompt(q, default=nil)
-    begin
-      puts "#{q}#{default && " [#{default}]"}"
+  def self.prompt(query, default = nil)
+    loop do
+      puts "#{query}#{default && " [#{default}]"}"
       answer = gets.chomp
       answer = answer.empty? && default || answer # set to default if answer is empty (and default is non nil)
-      accepted = block_given? && yield(answer)
-      puts "-- '#{answer}' is not acceptable. --" if block_given? && !accepted
-    end while block_given? && !accepted
+      break answer unless block_given?
 
-    answer
+      accepted = yield(answer)
+      break answer if accepted
+
+      puts "-- '#{answer}' is not acceptable. --"
+    end
   end
 end
